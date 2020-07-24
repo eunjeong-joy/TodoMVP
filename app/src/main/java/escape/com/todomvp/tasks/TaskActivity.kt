05e04d3ac8e585fb1ba7d217ae611793870a3995
eun jeong <chldmswjd55@gamil.com>
2020-07-24 15:45:06 +0900

    Mainactivity 제거하고 TaskActivity (Main), layout 추가

---
+++ app/src/main/java/escape/com/todomvp/tasks/TaskActivity.kt
@@ -0,0 +1,12 @@
+package escape.com.todomvp.tasks
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
