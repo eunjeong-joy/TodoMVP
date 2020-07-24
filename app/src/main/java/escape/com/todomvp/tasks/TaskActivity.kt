1f647f07aa24070c8f9fad6a5028a16638f3423c
eun jeong <chldmswjd55@gamil.com>
2020-07-24 18:06:44 +0900

    TaskActivity layout 구성, 연동

--- app/src/main/java/escape/com/todomvp/tasks/TaskActivity.kt
+++ app/src/main/java/escape/com/todomvp/tasks/TaskActivity.kt
@@ -2,11 +2,11 @@ package escape.com.todomvp.tasks
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import escape.com.todomvp.R
 
 class TaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
+        setContentView(R.layout.layout_task)
     }
 }
