42765459cc4279aeb57a377b6653e840254ed700
eun jeong <chldmswjd55@gamil.com>
2020-07-27 18:26:15 +0900

    statistics activity 등록.

---
+++ app/src/main/java/escape/com/todomvp/statistics/StatisticsActivity.kt
@@ -0,0 +1,10 @@
+package escape.com.todomvp.statistics
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StatisticsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
