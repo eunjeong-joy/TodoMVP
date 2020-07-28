65d7b3c51cb621a2862600fe75785745dcc91410
Choi Eunjeong <chldmswjd55@gmail.com>
2020-07-28 17:51:21 +0900

    TasksContract 내용 추가

--- app/src/main/java/escape/com/todomvp/tasks/TasksContract.kt
+++ app/src/main/java/escape/com/todomvp/tasks/TasksContract.kt
@@ -2,14 +2,22 @@ package escape.com.todomvp.tasks
 
 import escape.com.todomvp.BasePresenter
 import escape.com.todomvp.BaseView
+import escape.com.todomvp.data.Task
 
 interface TasksContract {
 
     interface View : BaseView<Presenter> {
 
+        var isActive: Boolean
+
     }
 
     interface Presenter : BasePresenter {
 
+        fun openTaskDetails(requestedTask: Task)
+
+        fun completedTask(completedTask: Task)
+
+        fun activateTask(activateTask: Task)
     }
 }
