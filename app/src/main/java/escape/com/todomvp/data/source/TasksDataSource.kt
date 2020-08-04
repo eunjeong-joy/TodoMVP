ceb608a8a1663667ac631dea46776298c8b9fe22
Choi Eunjeong <chldmswjd55@gmail.com>
2020-08-04 16:10:34 +0900

    TasksDataSource 추가.

---
+++ app/src/main/java/escape/com/todomvp/data/source/TasksDataSource.kt
@@ -0,0 +1,39 @@
+package escape.com.todomvp.data.source
+
+import escape.com.todomvp.data.Task
+
+interface TasksDataSource {
+    interface LoadTasksCallback {
+        fun onTasksLoaded(tasks: List<Task>)
+
+        fun onDataNotAvailable()
+    }
+
+    interface GetTaskCallback {
+        fun onTaskLoaded(task: Task)
+
+        fun onDataNotAvailable()
+    }
+
+    fun getTasks(callback: LoadTasksCallback)
+
+    fun getTask(taskId: String, callback: GetTaskCallback)
+
+    fun saveTask(task: Task)
+
+    fun completeTask(task: Task)
+
+    fun completeTask(taskId: String)
+
+    fun activateTask(task: Task)
+
+    fun activateTask(taskId: String)
+
+    fun clearCompletedTasks()
+
+    fun refreshTasks()
+
+    fun deleteAllTasks()
+
+    fun deleteTask(taskId: String)
+}
---
+++ app/src/main/java/escape/com/todomvp/data/source/local/TasksLocalDataSource.kt
@@ -0,0 +1,88 @@
+package escape.com.todomvp.data.source.local
+
+import androidx.annotation.VisibleForTesting
+import escape.com.todomvp.data.Task
+import escape.com.todomvp.data.source.TasksDataSource
+import escape.com.todomvp.util.AppExecutors
+
+class TasksLocalDataSource private constructor(
+    val appExecutors: AppExecutors,
+    val tasksDao: TasksDao
+) : TasksDataSource {
+
+    override fun getTasks(callback: TasksDataSource.LoadTasksCallback) {
+        appExecutors.diskIO.execute {
+            val tasks = tasksDao.getTasks()
+            appExecutors.mainThread.execute {
+                if(tasks.isEmpty()) {
+                    callback.onDataNotAvailable()
+                } else {
+                    callback.onTasksLoaded(tasks)
+                }
+            }
+        }
+    }
+
+    override fun getTask(taskId: String, callback: TasksDataSource.GetTaskCallback) {
+        appExecutors.diskIO.execute {
+            val task = tasksDao.getTaskById(taskId)
+            appExecutors.mainThread.execute {
+                if(task != null) {
+                    callback.onTaskLoaded(task)
+                } else {
+                    callback.onDataNotAvailable()
+                }
+            }
+        }
+    }
+
+    override fun saveTask(task: Task) {
+        appExecutors.diskIO.execute { tasksDao.insertTask(task) }
+    }
+
+    override fun completeTask(task: Task) {
+        appExecutors.diskIO.execute { tasksDao.updateCompleted(task.id, true) }
+    }
+
+    override fun completeTask(taskId: String) {}
+
+    override fun activateTask(task: Task) {
+        appExecutors.diskIO.execute { tasksDao.updateCompleted(task.id, false) }
+    }
+
+    override fun activateTask(taskId: String) {}
+
+    override fun clearCompletedTasks() {
+        appExecutors.diskIO.execute { tasksDao.deleteCompletedTasks() }
+    }
+
+    override fun refreshTasks() {}
+
+    override fun deleteAllTasks() {
+        appExecutors.diskIO.execute { tasksDao.deleteTasks() }
+    }
+
+    override fun deleteTask(taskId: String) {
+        appExecutors.diskIO.execute { tasksDao.deleteTaskById(taskId) }
+    }
+
+    companion object {
+        private var INSTANCE: TasksLocalDataSource? = null
+
+        @JvmStatic
+        fun getInstance(appExecutors: AppExecutors, tasksDao: TasksDao) :TasksLocalDataSource {
+            if(INSTANCE == null) {
+                synchronized(TasksLocalDataSource::javaClass) {
+                    INSTANCE = TasksLocalDataSource(appExecutors, tasksDao)
+                }
+            }
+
+            return INSTANCE!!
+        }
+
+        @VisibleForTesting
+        fun clearInstance() {
+            INSTANCE = null
+        }
+    }
+}
