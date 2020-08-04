c1196732ba69413be81130ec51674d2828ba46cd
Choi Eunjeong <chldmswjd55@gmail.com>
2020-08-04 18:17:06 +0900

    TaskContract View, Presenter에 기능 추가

--- app/src/main/java/escape/com/todomvp/tasks/TasksContract.kt
+++ app/src/main/java/escape/com/todomvp/tasks/TasksContract.kt
@@ -10,14 +10,56 @@ interface TasksContract {
 
         var isActive: Boolean
 
+        fun setLoadingIndicator(active: Boolean)
+
+        fun showTasks(tasks: List<Task>)
+
+        fun showAddTask()
+
+        fun showTaskDetailUi(taskId: String)
+
+        fun showTaskMarkedComplete()
+
+        fun showTaskMarkedActive()
+
+        fun showCompletedTasksClear()
+
+        fun showLoadingTasksError()
+
+        fun showNoTasks()
+
+        fun showActiveFilterLabel()
+
+        fun showCompletedFilterLabel()
+
+        fun showAllFilterLabel()
+
+        fun showNoActiveTasks()
+
+        fun showNoCompletedTasks()
+
+        fun showSuccessfullySavedMessage()
+
+        fun showFilteringPopUpMenu()
+
     }
 
     interface Presenter : BasePresenter {
+
+        var currentFiltering: TasksFilterType
+
+        fun result(requestCode: Int, resultCode: Int)
+
+        fun loadTasks(forceUpdate: Boolean)
+
+        fun addNewTask()
 
         fun openTaskDetails(requestedTask: Task)
 
         fun completedTask(completedTask: Task)
 
         fun activateTask(activateTask: Task)
+
+        fun clearCompletedTasks()
     }
 }
