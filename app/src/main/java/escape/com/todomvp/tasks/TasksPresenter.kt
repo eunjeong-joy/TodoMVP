0352e0085531919f6fb59791fb706b4c96317d9c
Choi Eunjeong <chldmswjd55@gmail.com>
2020-08-05 13:58:13 +0900

    Revert "tasksPresenter 추가"
    This reverts commit 4e2991876d3f5e2d2f03fd5b9ac5c87c59f6c4b3.

--- app/src/main/java/escape/com/todomvp/tasks/TasksPresenter.kt
+++
@@ -1,7 +0,0 @@
-package escape.com.todomvp.tasks
-
-import escape.com.todomvp.data.source.TasksRepository
-
-class TasksPresenter(val tasksRepository: TasksRepository, val tasksView: TasksContract.View) : TasksContract.Presenter {
-    override var currentFiltering = TasksFilterType.ALL_TASKS
-}
