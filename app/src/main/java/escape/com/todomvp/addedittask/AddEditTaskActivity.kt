669542e5497fe8d678c8d4184806e08a65c4eb69
Choi Eunjeong <chldmswjd55@gmail.com>
2020-08-05 13:55:23 +0900

    activity 추가 (taskDetail, add edit task)

---
+++ app/src/main/java/escape/com/todomvp/addedittask/AddEditTaskActivity.kt
@@ -0,0 +1,10 @@
+package escape.com.todomvp.addedittask
+
+import androidx.appcompat.app.AppCompatActivity
+
+class AddEditTaskActivity : AppCompatActivity() {
+    companion object {
+        const val SHOULD_LOAD_DATA_FROM_REPO_KEY = "SHOULD_LOAD_DATA_FROM_REPO_KEY"
+        const val REQUEST_ADD_TASK = 1
+    }
+}
---
+++ app/src/main/java/escape/com/todomvp/taskDetail/TaskDetailActivity.kt
@@ -0,0 +1,9 @@
+package escape.com.todomvp.taskDetail
+
+import androidx.appcompat.app.AppCompatActivity
+
+class TaskDetailActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_TASK_ID = "TASK_ID"
+    }
+}
