3b769143425026d35a87f55042765912d86a056e
Choi Eunjeong <chldmswjd55@gmail.com>
2020-08-11 23:55:30 +0900

    Util fun 추가

--- app/src/main/java/escape/com/todomvp/util/AppCompatActivityExt.kt
+++ app/src/main/java/escape/com/todomvp/util/AppCompatActivityExt.kt
@@ -3,8 +3,23 @@ package escape.com.todomvp.util
 import androidx.annotation.IdRes
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 fun AppCompatActivity.setupActionBar(@IdRes toolbarId: Int, action: ActionBar.() -> Unit) {
     setSupportActionBar(findViewById(toolbarId))
     supportActionBar?.run { action() }
+}
+
+fun AppCompatActivity.replaceFragmentInActivity(fragment: Fragment, @IdRes frameId: Int) {
+    supportFragmentManager.transact {
+        replace(frameId, fragment)
+    }
+}
+
+private inline fun FragmentManager.transact(action: FragmentTransaction.() -> Unit) {
+    beginTransaction().apply{
+        action()
+    }.commit()
 }
