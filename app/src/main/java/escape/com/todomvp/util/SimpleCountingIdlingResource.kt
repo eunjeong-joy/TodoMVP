300b19e161d85f512f16aeb960e7c5b8045ace40
Choi Eunjeong <chldmswjd55@gmail.com>
2020-08-26 21:59:35 +0900

    espresso의 IdlingResource 상속받은 SimpleCountingIdlingResource 추가

---
+++ app/src/main/java/escape/com/todomvp/util/SimpleCountingIdlingResource.kt
@@ -0,0 +1,35 @@
+package escape.com.todomvp.util
+
+import androidx.test.espresso.IdlingResource
+import java.util.concurrent.atomic.AtomicInteger
+
+
+class SimpleCountingIdlingResource(val resourceName: String) : IdlingResource {
+
+    private val counter = AtomicInteger(0)
+
+    @Volatile private var resourceCallBack: IdlingResource.ResourceCallback? = null
+
+    override fun getName() = resourceName
+
+    override fun isIdleNow() = counter.get() == 0
+
+    override fun registerIdleTransitionCallback(callback: IdlingResource.ResourceCallback?) {
+        this.resourceCallBack = callback
+    }
+
+    fun increment() {
+        counter.getAndIncrement()
+    }
+
+    fun decrement() {
+        val counterVal = counter.getAndDecrement()
+        if(counterVal == 0) {
+            resourceCallBack.onTransitionToIdle()
+        }
+
+        if(counterVal < 0) {
+            throw IllegalArgumentException("Counter has been corrupted!")
+        }
+    }
+}
